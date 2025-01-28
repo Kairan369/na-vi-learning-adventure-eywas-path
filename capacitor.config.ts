6e053e270db5329a5193b6fcccc0aa9c1612fd98
Kairan369 <kairan.johnson369@gmail.com>
2025-01-28 23:50:29 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: "Na'vi Learning Adventure: Eywa's Path",
+  webDir: 'dist'
+};
+
+export default config;
